9ba2eefad016d1b39f70519bb94762c397cfd070
Marleen <marleeneijkemans@gmail.com>
2025-03-11 20:38:47 +0100

    add better hover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { StorageComponent } from "./storage/storage.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterModule, NavbarComponent, StorageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StorageComponent } from './storage/storage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'storage', component: StorageComponent },
+  { path: '', redirectTo: '/storage', pathMatch: 'full' }
+];
