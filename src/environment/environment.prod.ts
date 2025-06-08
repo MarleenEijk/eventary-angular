9bcc9f740bc056375b75bc6d7f4151e941df0eb8
Marleen <marleeneijkemans@gmail.com>
2025-06-08 21:20:58 +0200

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     isProduction: true,
-    apiUrl: 'https://eventary-frontend.internal.victoriousrock-cc8323fc.northeurope.azurecontainerapps.io'
+    apiUrl: 'https://eventary-api.victoriousrock-cc8323fc.northeurope.azurecontainerapps.io'
 }
